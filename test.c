281eb0ca076ee6080f56e774c41e8568baf43d87
unknown <ibrahim.nmga@gmail.com>
2022-12-23 21:17:03 +0100

    test

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int ac, char av)
+{
+
+ int noombre_de_vie = 5, nivau = 1;
+
+ printf("il vous reste %d nombre de vie et vous etes au niv %d", noombre_de_vie, nivau);
+
+ return 0;
+
+}
